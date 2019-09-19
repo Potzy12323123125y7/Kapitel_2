4505782a352951cedb6066eed9cb266844a72db5
Alexander Jonsson Näslund <alexander.naslund4@learnet.se>
2019-09-19 11:17:57 +0200

    upgift2_6

---
+++ upgift_2_6.java
@@ -0,0 +1,21 @@
+/*
+ Ett enkelt Hello World i java
+ Skapat 2019-09-19
+ Programmerare Alexander Jonsson Näslund
+*/
+
+import javax.swing.*;
+import java.util.Scanner;
+
+
+public class upgift_2_6 { 
+	public static void main(String[] args) {
+		char c1 ='\u00D8';
+		char c2 ='\u00F8';
+		char c3 = '\u03C0';
+		JOptionPane.showMessageDialog(null,""+c1);
+		JOptionPane.showMessageDialog(null,""+c2);
+		JOptionPane.showMessageDialog(null,""+c3);
+	
+	}
+}
